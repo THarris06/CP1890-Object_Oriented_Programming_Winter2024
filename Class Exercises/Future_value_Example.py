80e59855152ffb985a863db362c912b85a4e8583
Matthew King <146943749+MattEKing@users.noreply.github.com>
2024-01-12 10:03:06 -0330

    Future calc example. Class 3

---
+++ Class Exercises/Future_value_Example.py
@@ -0,0 +1,30 @@
+import locale as lc
+def get_values():
+    invest = float(input("Enter the investment:\t\t "))
+    rate = float(input("Enter Yearly interest rate:\t "))
+    years = int(input("Enter number of years:\t\t "))
+    return invest, rate, years
+
+
+def calculate_values(invest, rate, years):
+    future_value = 0
+    for i in range(years*12):
+        future_value += invest
+        monthly = future_value * (rate/(12*100))
+        future_value += monthly
+    return future_value
+
+
+def main():
+    print('Future Value Calculator')
+    invest, rate, years = get_values()
+    calculated_value = calculate_values(invest, rate, years)
+    lc.setlocale(lc.LC_ALL, 'en-ca')
+    print(f"\nmonthly investment:{lc.currency(invest, grouping=True):>13}")
+    print(f"Interest rate:{rate:>18}")
+    print(f"Years:{years:>26}")
+    print(f"Future value:{lc.currency(calculated_value, grouping=True):>19}")
+
+
+if __name__ == '__main__':
+    main()
