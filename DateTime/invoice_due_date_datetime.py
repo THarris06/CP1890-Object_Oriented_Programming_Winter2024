053018167b39f5a8c3dae4dbb69b68cdf50ce01b
Matthew King <146943749+MattEKing@users.noreply.github.com>
2024-02-07 09:42:45 -0330

    files update

--- DateTime/invoice_due_date_datetime.py
+++ DateTime/invoice_due_date_datetime.py
@@ -8,7 +8,7 @@ def title():
 def calc_dates(invoice_date, current_date):
     days_till = timedelta(days=30)
     due = invoice_date + days_till
-    days_late = current_date - due
+    days_late = (current_date - due).days
     return due, days_late
 
 
@@ -21,7 +21,7 @@ def main():
         print(f"\n{'Invoice date:':13}{invoice_date: %B %d,%Y}")
         print(f"{'Due date:':13}{due_date: %B %d,%Y}")
         print(f"{'Current date:':13}{current_date: %B %d,%Y}")
-        print(f"\nThis invoice is {late.days} day(s) overdue.")
+        print(f"\nThis invoice is {late} day(s) overdue.")
         choice = input("\ncontinue? (y/n):")
         if choice == "y":
             continue
