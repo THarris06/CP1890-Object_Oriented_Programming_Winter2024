7ac73d64d2dcd2a9bb8000b7e4daaed32d3484a4
Matthew King <146943749+MattEKing@users.noreply.github.com>
2024-02-09 09:42:55 -0330

    changed inputs for arrival anf departure and fixed locale currency

--- DateTime/hotel_exservation.py
+++ DateTime/hotel_exservation.py
@@ -1,8 +1,46 @@
+
 from datetime import datetime
+import locale as lc
+lc.setlocale(lc.LC_ALL, 'en_CA')
+
+
+'''Code of getting all Locale codes
+   for loc in locale.locale_alias:
+      print(loc)'''
 
 
 def title():
     print("Hotel Hotel Reservation Program\n")
+
+
+def get_arrival():
+    while True:
+        try:
+            arrival_date = datetime.strptime(input('Enter arrival date (YYYY-MM-DD): '), "%Y-%m-%d")
+        except ValueError as ve:
+            print('\nInvalid date of Arrival , Enter a valid date.\n')
+            print(f"Error:{ve}")
+            continue
+        now = datetime.now()
+        if arrival_date < now:
+            print("This date has pasted, arrival date needs to be today or later.\n")
+        else:
+            return arrival_date
+
+
+def get_departure(arrival):
+    while True:
+        try:
+            departure_date = datetime.strptime(input('Enter departure date (YYYY-MM-DD): '), "%Y-%m-%d")
+        except ValueError as ve:
+            print('\nInvalid date of departure , Enter a valid date.\n')
+            print(f"Error:{ve}\n")
+            continue
+        now = datetime.now()
+        if departure_date < arrival:
+            print("This date has pasted your arrival, date needs to be later then arrival.\n")
+        else:
+            return departure_date
 
 
 def calc_night_rate(arrival, departure):
@@ -18,21 +56,18 @@ def calc_night_rate(arrival, departure):
 def main():
     title()
     while True:
-        try:
-            arrival = datetime.strptime(input('Enter arrival date (YYYY-MM-DD): '), "%Y-%m-%d")
-            departure = datetime.strptime(input('Enter departure date (YYYY-MM-DD): '), '%Y-%m-%d')
-            night_rate, total_nights, total_price = calc_night_rate(arrival, departure)
-            print(f"\n{'Arrival Date:':<16}{arrival:%B %d, %Y}")
-            print(f"{'Departure Date:':<16}{departure:%B %d, %Y}")
-            if night_rate == 105.00:
-                print(f"{'Nightly rate:':<16}${night_rate:.2f} (High season)")
-            else:
-                print(f"{'Nightly rate:':<16}${night_rate:.2f}")
-            print(f"{'Total nights:':<16}{total_nights}")
-            print(f"{'Total price:':<16}${total_price:.2f}")
-        except ValueError:
-            print('\nInvalid date of Arrival or Departure, Enter a valid date for both.\n')
-            continue
+        arrival = get_arrival()
+        departure = get_departure(arrival)
+        night_rate, total_nights, total_price = calc_night_rate(arrival, departure)
+        print(f"\n{'Arrival Date:':<16}{arrival:%B %d, %Y}")
+        print(f"{'Departure Date:':<16}{departure:%B %d, %Y}")
+        if night_rate == 105.00:
+            print(f"{'Nightly rate:':<16}{lc.currency(night_rate, symbol=True, grouping=True)} (High season)")
+        else:
+            print(f"{'Nightly rate:':<16}${lc.currency(night_rate, symbol=True, grouping=True)}")
+        print(f"{'Total nights:':<16}{total_nights}")
+        print(f"{'Total price:':<16}{lc.currency(total_price, symbol=True, grouping=True)}")
+
         choice = input("\ncontinue? (y/n): ")
         if choice == 'y':
             continue
